23b98dbc9df0c543e496cf16db9f64b90c9d594a
tejojr <ammar.fiky@gmail.com>
2019-11-22 12:45:35 +0700

    init task1

---
+++ task1/number1.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} beginWord
+ * @param {string} endWord
+ * @param {string[]} wordList
+ * @return {string[][]}
+ */
+var findLadders = function(beginWord, endWord, wordList) {}
+
+// case1
+let beginWord = 'hit'
+let endWord = 'cog'
+const wordList = ['hot', 'dot', 'dog', 'lot', 'log', 'cog']
+// // case2
+// let beginWord = 'hit'
+// let endWord = 'cog'
+// const wordList = ['hot', 'dot', 'dog', 'lot', 'log']
+console.log(findLadders(beginWord, endWord, wordList))
---
+++ task1/number2.js
@@ -0,0 +1,11 @@
+const maxNumberPoints = coordinate => {}
+
+// -> di soal no2 tidak ada aturan strukturnya
+// case1
+let input = [
+    [1, 1],
+    [2, 2],
+    [3, 3],
+]
+
+console.log(maxNumberPoints(input))
